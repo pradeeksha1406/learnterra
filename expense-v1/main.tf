116bd6240abd0bbaa135a6e6d119b3fdacdec531
pradeep <p.guta@gmail.com>
2024-11-28 16:04:21 +0530

    Ec2

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.ami.id
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-098eb31481a73cb9c"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "frontend"
@@ -19,7 +19,7 @@ resource "aws_route53_record" "frontend" {
 resource "aws_instance" "backend" {
   ami           = data.aws_ami.ami.id
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-098eb31481a73cb9c"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "backend"
@@ -37,7 +37,7 @@ resource "aws_route53_record" "backend" {
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.ami.id
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-098eb31481a73cb9c"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "mysql"
