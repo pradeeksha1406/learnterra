368d1448fb7b423d27695f7088b7ff2ae2be03eb
pradeep <p.guta@gmail.com>
2024-11-28 15:54:08 +0530

    Ec2

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-0b4f379183e5706b9"
+  ami           = data.aws_ami.ami.
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-098eb31481a73cb9c"]
 
