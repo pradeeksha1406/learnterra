6c8b946ac01ac7fa8a760328af0d78c3c9e64c4e
pradeep <p.guta@gmail.com>
2024-11-29 10:31:28 +0530

    Ec2

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,18 +1,10 @@
 resource "aws_instance" "frontend" {
-  ami           = data.aws_ami.ami.id
-  instance_type = "t2.micro"
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "frontend"
-  }
-
-  provisioner "local-exec" {
-    command = <<EOF
-cd /etc/
-cat passwd | grep root
-EOF
-
   }
 }
 
@@ -22,6 +14,16 @@ resource "aws_route53_record" "frontend" {
   type    = "A"
   ttl     = 300
   records = [aws_instance.frontend.private_ip]
+}
+
+resource "null_resource" "frontend" {
+  depends_on = [aws_route53_record.frontend]
+  provisioner "local-exec" {
+    command = <<EOF
+       cd /etc
+       cat passwd | grep root
+EOF
+  }
 }
 
 resource "aws_instance" "backend" {
@@ -42,6 +44,16 @@ resource "aws_route53_record" "backend" {
   records = [aws_instance.backend.private_ip]
 }
 
+resource "null_resource" "backend" {
+  depends_on = [aws_route53_record.backend]
+  provisioner "local-exec" {
+    command = <<EOF
+       cd /etc
+       cat passwd | grep root
+EOF
+  }
+}
+
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.ami.id
   instance_type = "t2.micro"
@@ -58,4 +70,14 @@ resource "aws_route53_record" "mysql" {
   type    = "A"
   ttl     = 300
   records = [aws_instance.mysql.private_ip]
+}
+
+resource "null_resource" "mysql" {
+  depends_on = [aws_route53_record.mysql]
+  provisioner "local-exec" {
+    command = <<EOF
+       cd /etc
+       cat passwd | grep root
+EOF
+  }
 }
