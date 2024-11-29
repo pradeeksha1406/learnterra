29e043469af60ee42e4bdfdf746470e5f17c1fed
pradeep <p.guta@gmail.com>
2024-11-29 12:23:29 +0530

    Ec2

--- expense-v2/main.tf
+++ expense-v2/main.tf
@@ -2,3 +2,13 @@ module "frontend" {
   source = "./app-create"
   component = "frontend"
 }
+
+#module "backend" {
+#  source = "./app-create"
+#  component = "frontend"
+#}
+#
+#module "mysql" {
+#  source = "./app-create"
+#  component = "mysql"
+#}
